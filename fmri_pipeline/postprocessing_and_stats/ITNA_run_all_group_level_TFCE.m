5fe640af356814039664e4c12c92694170a463e7
NBL iMac 2 <nbl_imac2@NBLs-iMac.local>
2021-01-11 18:30:11 -0600

    add script to run all contrasts through TFCE

---
+++ fmri_pipeline/postprocessing_and_stats/ITNA_run_all_group_level_TFCE.m
@@ -0,0 +1,173 @@
+%% Run Threshold Free Cluster Enhancement (TFCE) significance testing for all pairwise contrasts
+purge
+niters = 100000; % Total number of iterations for TFCE
+test = 2; % All of the tests here are paired two-sample tests
+data_dir = '/Volumes/NBL_Projects/Price_NFA/Analyses_for_Paper/AllSubs_dsets';
+out_dir = '/Volumes/NBL_Projects/Price_NFA/Analyses_for_Paper/Results';       
+
+% Define the inputs for two-sample testing, specifying the label for the
+% contrast, then the group mask, then the two datasets for comparison (N
+% contasts x 3 cell array). Each dataset file is a 4D dataset with every
+% subject's connectivity map catenated into one file.
+
+input_strings = {
+    'PairedTest_SC_DigLH_vs_LetLH_log+c','lh','ld141',...
+    'GroupMask_DigLH_LetLH_ConsThr0.3_ld141.niml.dset',...
+        'AllSubs_tracks_ss3t_50M_Dp-Da.lh.TDI_ends.norm.al2anat.lh.6mm.log+c.niml.dset',...
+        'AllSubs_tracks_ss3t_50M_Lp-La.lh.TDI_ends.norm.al2anat.lh.6mm.log+c.niml.dset';
+        
+    'PairedTest_SC_DigLH_vs_DigRH_log+c_on_LHsurf','lh','ld141',...
+    'GroupMask_DigLH_DigRH_ConsThr0.3_on_LHsurf_ld141.niml.dset',...
+        'AllSubs_tracks_ss3t_50M_Dp-Da.lh.TDI_ends.norm.al2anat.lh.6mm.log+c.niml.dset',...
+        'AllSubs_tracks_ss3t_50M_Dp-Da_math.rh.TDI_ends.norm.al2anat.rh.6mm.log+c_MAP2CON.niml.dset';
+        
+    'PairedTest_SC_DigLH_vs_DigRH_log+c_on_RHsurf','rh','ld141',...
+    'GroupMask_DigLH_DigRH_ConsThr0.3_on_RHsurf_ld141.niml.dset',...
+        'AllSubs_tracks_ss3t_50M_Dp-Da.lh.TDI_ends.norm.al2anat.lh.6mm.log+c_MAP2CON.niml.dset',...
+        'AllSubs_tracks_ss3t_50M_Dp-Da_math.rh.TDI_ends.norm.al2anat.rh.6mm.log+c.niml.dset';
+        
+    'PairedTest_FC_Dp_DigLH_vs_LetLH','lh','ld60',...
+    'GroupMask_DigLH_LetLH_ld60.niml.dset',...
+        'AllSubs_Dp-Da.lh.beta_series_corr.lh.Zmap.Dp.niml.dset',...
+        'AllSubs_Lp-La.lh.beta_series_corr.lh.Zmap.Dp.niml.dset';
+            
+    'PairedTest_FC_Dp-Da_DigLH_vs_LetLH','lh','ld60',...
+    'GroupMask_DigLH_LetLH_ld60.niml.dset',...
+        'AllSubs_Dp-Da.lh.beta_series_corr.lh.Zdiff.Dp-Da.niml.dset',...
+        'AllSubs_Lp-La.lh.beta_series_corr.lh.Zdiff.Dp-Da.niml.dset';
+        
+    'PairedTest_FC_Dp_DigLH_vs_Lp_LetLH','lh','ld60',...
+    'GroupMask_DigLH_LetLH_ld60.niml.dset',...
+        'AllSubs_Dp-Da.lh.beta_series_corr.lh.Zmap.Dp.niml.dset',...
+        'AllSubs_Lp-La.lh.beta_series_corr.lh.Zmap.Lp.niml.dset';
+        
+    'PairedTest_FC_Dp-Da_DigLH_vs_Lp-La_LetLH','lh','ld60',...
+    'GroupMask_DigLH_LetLH_ld60.niml.dset',...
+        'AllSubs_Dp-Da.lh.beta_series_corr.lh.Zdiff.Dp-Da.niml.dset',...
+        'AllSubs_Lp-La.lh.beta_series_corr.lh.Zdiff.Lp-La.niml.dset';
+        
+    'PairedTest_FC_Dp_DigLH_vs_DigRH_on_LHsurf','lh','ld60',...
+    'GroupMask_DigLH_DigRH_on_LHsurf_ld60.niml.dset',...
+        'AllSubs_Dp-Da.lh.beta_series_corr.lh.Zmap.Dp.niml.dset',...
+        'AllSubs_Dp-Da_math.rh.beta_series_corr.rh.Zmap.Dp_MAP2CON.niml.dset';
+        
+    'PairedTest_FC_Dp-Da_DigLH_vs_DigRH_on_LHsurf','lh','ld60',...
+    'GroupMask_DigLH_DigRH_on_LHsurf_ld60.niml.dset',...
+        'AllSubs_Dp-Da.lh.beta_series_corr.lh.Zdiff.Dp-Da.niml.dset',...
+        'AllSubs_Dp-Da_math.rh.beta_series_corr.rh.Zdiff.Dp-Da_MAP2CON.niml.dset';
+    
+    'PairedTest_FC_Dp_DigLH_vs_DigRH_on_RHsurf','rh','ld60',...
+    'GroupMask_DigLH_DigRH_on_RHsurf_ld60.niml.dset'...
+        'AllSubs_Dp-Da.lh.beta_series_corr.lh.Zmap.Dp_MAP2CON.niml.dset',...
+        'AllSubs_Dp-Da_math.rh.beta_series_corr.rh.Zmap.Dp.niml.dset';
+        
+    'PairedTest_FC_Dp-Da_DigLH_vs_DigRH_on_RHsurf','rh','ld60',...
+    'GroupMask_DigLH_DigRH_on_RHsurf_ld60.niml.dset'...
+        'AllSubs_Dp-Da.lh.beta_series_corr.lh.Zdiff.Dp-Da_MAP2CON.niml.dset',...
+        'AllSubs_Dp-Da_math.rh.beta_series_corr.rh.Zdiff.Dp-Da.niml.dset'};
+
+% Loop through and run each contrast through TFCE
+cd(data_dir)
+for ii = 1:size(input_strings,1)
+    % Get the label for this test 
+    label = [out_dir '/' input_strings{ii,1}];
+    hemi = input_strings{ii,2};
+    density = input_strings{ii,3};
+    mask_dset = input_strings{ii,4};
+    
+    % Create mean datasets for inspection/visualizaton
+    unix(['3dTstat -mean -overwrite -prefix ' label '_SET1_MEAN '...
+        input_strings{ii,5}]);
+    unix(['3dTstat -mean -overwrite -prefix ' label '_SET2_MEAN '...
+        input_strings{ii,6}]);
+    
+    % Combine all data into one input file
+    unix(['3dTcat -overwrite -prefix ' label '_ALLDATA '...
+        input_strings{ii,5} ' ' input_strings{ii,6}]);
+    
+    in_dset = [label '_ALLDATA.niml.dset'];
+    
+    % Set filename for the output statistic dataset and number of iterations
+    out = [label '_TFCE_Zscore_' num2str(niters) 'iters_MASK.niml.dset'];
+    
+    % Specify no input null (i.e. use the program's default method)
+    out_dset_null = '';
+    
+    % Call global function
+    setup_run_TFCE(in_dset,mask_dset,niters,out,test,hemi,density,out_dset_null)
+end
+
+
+%% Global function
+function [] = setup_run_TFCE(in_dset,mask_dset,niters,out,test,hemi,density,out_dset_null)
+
+% Load in the subject data
+surf_ds = afni_niml_readsimple(in_dset);
+% Mask the subject data
+mask_ds = afni_niml_readsimple(['/Users/nbl_imac2/Documents/GitHub/ITNA_Connectivity/roi_creation/rois/' mask_dset]);
+surf_ds.data = surf_ds.data .* mask_ds.data;
+
+% Convert to COSMOMVPA formatted structure
+surf_ds.labels = num2cell(1:size(surf_ds.data,2));
+surf_ds = cosmo_surface_dataset(surf_ds);
+
+% Get faces and vertices info from MNI surface gii file
+if strcmp(density,'ld60')
+    surf_gii = ['/Users/nbl_imac2/Desktop/Price_NFA_Tractography_Surface/suma_MNI152_2009/std.60.' hemi '.smoothwm.gii'];
+elseif strcmp(density,'ld141')
+    surf_gii = ['/Users/nbl_imac2/Desktop/Price_NFA_Tractography_Surface/suma_MNI152_2009/std.141.' hemi '.smoothwm.gii'];
+end
+
+[vertices,faces]=surfing_read(surf_gii);
+faces = double(faces);
+vertices = double(vertices);
+
+% Set up surface dataset structure, to prepare it for Cosmo TFCE
+surf_ds.fa.center_ids = surf_ds.fa.node_indices;
+if test == 2
+    % Two-sample test
+    nsubs = size(surf_ds.samples,1)/2;
+    surf_ds.sa.chunks = [(1:nsubs)';(1:nsubs)'];
+    surf_ds.sa.targets = [ones(nsubs,1);2*ones(nsubs,1)];
+elseif test == 1
+    % One-sample test (vs 0)
+    nsubs = size(surf_ds.samples,1);
+    surf_ds.sa.chunks = (1:nsubs)';
+    surf_ds.sa.targets = ones(nsubs,1);
+end
+
+% Set up null surface data structure
+surf_dset_null = cell(1,100);
+if strcmp(out_dset_null,'') ~= 1
+    null = afni_niml_readsimple(out_dset_null);
+    ds = surf_ds;
+    for ii = 1:100
+        ds.samples = null.data(:,ii:100:size(null.data,2))';
+        surf_dset_null(ii) = {ds};
+    end
+else
+    surf_dset_null = [];
+end
+
+% Run TFCE
+[~] = ITNA_run_TFCE(surf_ds,vertices,faces,niters,out,surf_dset_null);
+
+% Also get the uncorrected z-scores for verification 
+% (NOTE this is adapted from last part of ITNA_run_TFCE)
+stat_ds = cosmo_stat(surf_ds,'t','z');
+fprintf('TFCE z-score dataset\n');
+cosmo_disp(stat_ds);
+nfeatures=size(stat_ds.samples,2);
+percentiles=(1:nfeatures)/nfeatures*100;
+plot(percentiles,sort(stat_ds.samples))
+title('sorted TFCE z-scores');
+xlabel('feature percentile');
+ylabel('z-score');
+nvertices=size(vertices,1);
+disp_opt=struct();
+disp_opt.DataRange=[-2 2];
+DispIVSurf(vertices,faces,1:nvertices,stat_ds.samples',0,disp_opt);
+out_unc = strrep(out,'.niml.dset','_uncorrZscr.niml.dset');
+cosmo_map2surface(stat_ds,out_unc);
+
+end
